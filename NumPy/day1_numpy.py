ded6105edba42c24c5720087f7db6e1de51dfe8b
Arshpreet3105kaur <arshpreet3105kaur@gmail.com>
2026-06-30 00:46:09 +0530

    Add NumPy array creation and basic operations

--- NumPy/day1_numpy.py
+++ NumPy/day1_numpy.py
@@ -1,1 +1,7 @@
-
+import numpy as np 
+arr=np.array([10,20,30,40,50])
+print("array:",arr)
+print("type: ",type(arr))
+print("First element: ",arr[0])
+print("Last element: ",arr[-1])
+print("Length: ",len(arr))
